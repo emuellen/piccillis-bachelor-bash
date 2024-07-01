731ac1924140da774ff1ecc854c3a831a13f2126
Eric Muellenbach <emuellenbach@gmx.de>
2024-07-01 12:03:02 -0700

    Add top logo and better video rendering on page

--- src/VideoSelector.js
+++ src/VideoSelector.js
@@ -36,53 +36,17 @@ const VideoSelector = () => {
       });
       setVideoUrl(response.data.url);
     } catch (error) {
-        setYoutubeUrl("aabb");
+        setYoutubeUrl("https://www.youtube.com/embed/dQw4w9WgXcQ?si=eHQqRxzl9RyaaDfm");
     }
   };
 
-  if (videoUrl) {
-    return (
-      <div className="video-container">
-        <div className="background-slideshow">
-        {backgroundImages.map((image, index) => (
-          <div
-            key={index}
-            className={`background-slide ${index === currentBackground ? 'active' : ''}`}
-            style={{ backgroundImage: `url(${image})` }}
-          ></div>
-        ))}
-      </div>
-        <iframe
-          src={videoUrl}
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          autoPlay
-          title="Video"
-        ></iframe>
-      </div>
-    );
-  }
-
-  if (youtubeUrl) {
-    return (
-      <div className="video-container">
-        <div className="background-slideshow">
-        {backgroundImages.map((image, index) => (
-          <div
-            key={index}
-            className={`background-slide ${index === currentBackground ? 'active' : ''}`}
-            style={{ backgroundImage: `url(${image})` }}
-          ></div>
-        ))}
-      </div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=eHQqRxzl9RyaaDfm?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      </div>
-    );
-  }
-
   return (
-    <div className="selector-container">
+    <>
+    <div className="logo">
+        <img src="./images/logo.png" alt="Logo" />
+      </div>
+        <div className="selector-container">
+      
       <div className="background-slideshow">
         {backgroundImages.map((image, index) => (
           <div
@@ -92,44 +56,60 @@ const VideoSelector = () => {
           ></div>
         ))}
       </div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            What teams played in the Europa League on the 5th of April 2018?
-            <select value={option1} onChange={(e) => setOption1(e.target.value)}>
-              <option value="">Select...</option>
-              <option value="option1A">1. FC Köln - Bayern München</option>
-              <option value="option1B">RB Leipzig - Olympique de Marseille</option>
-              <option value="option1C">Fluminense FC - SE Palmeiras</option>
-            </select>
-          </label>
+      {videoUrl || youtubeUrl ? (
+        <div className="video-container">
+          <div className="video-wrapper">
+            <iframe
+              src={videoUrl || youtubeUrl}
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              autoPlay
+              allowFullScreen
+              title="Video"
+            ></iframe>
+          </div>
         </div>
-        <div>
-          <label>
-            What costume did Imke wear Halloween 2019?
-            <select value={option2} onChange={(e) => setOption2(e.target.value)}>
-              <option value="">Select...</option>
-              <option value="option2A">Giraffe</option>
-              <option value="option2B">Vampire Zombie</option>
-              <option value="option2C">Penguin</option>
-            </select>
-          </label>
-        </div>
-        <div>
-          <label>
-            Which words were displayed on the wall in La Cocina Negra?
-            <select value={option3} onChange={(e) => setOption3(e.target.value)}>
-              <option value="">Select...</option>
-              <option value="option3A">Você é uma gatinha</option>
-              <option value="option3B">Solo la puntita</option>
-              <option value="option3C">De Puta Madre</option>
-            </select>
-          </label>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      ) : (
+        <form onSubmit={handleSubmit}>
+        <div className="form-field">
+            <label>
+              What teams played in the Europa League on the 5th of April 2018?
+              <select value={option1} onChange={(e) => setOption1(e.target.value)}>
+                <option value="">Select...</option>
+                <option value="option1A">1. FC Köln - Bayern München</option>
+                <option value="option1B">RB Leipzig - Olympique de Marseille</option>
+                <option value="option1C">Fluminense FC - SE Palmeiras</option>
+              </select>
+            </label>
+          </div>
+          <div className="form-field">
+            <label>
+              What costume did Imke wear Halloween 2019?
+              <select value={option2} onChange={(e) => setOption2(e.target.value)}>
+                <option value="">Select...</option>
+                <option value="option2A">Giraffe</option>
+                <option value="option2B">Vampire Zombie</option>
+                <option value="option2C">Penguin</option>
+              </select>
+            </label>
+          </div>
+          <div className="form-field">
+            <label>
+              Which words were displayed on the wall in La Cocina Negra?
+              <select value={option3} onChange={(e) => setOption3(e.target.value)}>
+                <option value="">Select...</option>
+                <option value="option3A">Você é uma gatinha</option>
+                <option value="option3B">Solo la puntita</option>
+                <option value="option3C">De Puta Madre</option>
+              </select>
+            </label>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
-  );
+    </>
+    ); 
 };
 
 export default VideoSelector;
